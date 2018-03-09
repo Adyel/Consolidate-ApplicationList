b6d086509a803fee9437a0ea0ef016a89d6389de
Adyel <adyel@live.com>
2018-03-09 13:55:37 +0600

    Changed from Scanner to BufferRead

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,53 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        File file = new File("SampleData.txt");
+        //File file = new File("SampleData.txt");
         //File file = new File("ADVENT.txt");
         Scanner cin = null;
 
+        FileReader file = null;
+        try {
+
+            //file = new FileReader("SampleData.txt");
+            file = new FileReader("ADVENT.txt");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        BufferedReader br = new BufferedReader(file);
+
+
+/*
         try {
             cin = new Scanner(file);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
-        SQLConnection sql = new SQLConnection();
+
 
         //sql.initialise();
+
+     */
+        SQLConnection sql = new SQLConnection();
         sql.createDatabase();
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            //System.out.println(line);
+            sql.insertData( line );
+        }
+
+        /*
         while ( cin.hasNext() ){
             sql.insertData( cin.next() );
         }
+        */
+
 
     }
 
